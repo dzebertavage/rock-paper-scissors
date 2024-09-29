1bb978b3a522eec6e91abf85f976eed347d55a72
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2024-09-29 14:49:32 -0400

    Start writing pseudocode as comments in js file

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+//  Write function that returns "rock" "paper" or "scissors"
+    //  
+//  Write function that gets user input — "rock" "paper" or "scissors"
+
+//  Track players' scores in variables
+
+//  Write function for a single round of play
+
+//  Write logic to play an entire game (5 rounds)
