aaf1d11cb2bab3bda40c4f88d9929598d3d85151
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2024-10-01 17:06:54 -0400

    Update to pseudocode

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,15 +1,5 @@
-
-
-
-
-
-
-//  Write function that returns "rock" "paper" or "scissors"
-    //  
+//  Write function that returns "rock" "paper" or "scissors" as computer's choice
 //  Write function that gets user input — "rock" "paper" or "scissors"
-
-//  Track players' scores in variables
-
+//  Write function that tracks score
 //  Write function for a single round of play
-
 //  Write logic to play an entire game (5 rounds)
