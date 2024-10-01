5daa0b804d4f5d5e3f22912d0407457324a4bf08
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2024-10-01 17:30:36 -0400

    Make function randomly pick rock, paper, scissors

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,3 +1,19 @@
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+let computerChoice = getComputerChoice();
+
+
+
+//  Pseudocode:
 //  Write function that returns "rock" "paper" or "scissors" as computer's choice
 //  Write function that gets user input — "rock" "paper" or "scissors"
 //  Write function that tracks score
